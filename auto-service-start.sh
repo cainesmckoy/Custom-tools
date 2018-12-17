e2dd3c99be92f8a34e1e7924af64057a843c89ba
cainesmckoy <caine12393@yahoo.com>
2018-12-17 00:33:52 -0500

    Update auto-service-start.sh

--- auto-service-start.sh
+++ auto-service-start.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
+#this is a sub-script to start up any Service
+#caine munro
+DEFAULT=default 
 
+function startup {
+    variables=${1-DEFAULT};
+    serv="service bind9 status ";
+    $serv  > fuckshit.txt ;
+    stat="grep dead fuckshit.txt";
+    $stat;
+    match="grep --only-matching dead fuckshit.txt";
+   
+    if [[ "$match"=="true" ]]
+    then
+        service bind9 start
+        sleep 1
+        service bind9 status
+    fi;
+
+}
+startup
+
+exit
 
